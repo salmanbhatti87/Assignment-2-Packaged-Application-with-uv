baafdf5db332a1e119886e47ddd235c8a069120c
salmanbhatti87 <114308008+salmanbhatti87@users.noreply.github.com>
2025-08-30 11:22:47 +0000

    Assignment-2-Packaged-Application-with-uv

---
+++ my-package-app/src/my_package_app/__init__.py
@@ -0,0 +1,2 @@
+def main() -> None:
+    print("Hello from my-package-app!")
---
+++ my-package-app/src/my_package_app/main.py
@@ -0,0 +1,4 @@
+print("Top-level hello from main.py!")  # runs immediately when file is executed or imported
+
+def main() -> None:
+    print("Hello from my-packaged-app! — running main.py")
